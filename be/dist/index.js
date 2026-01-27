38d481ac32a789e49617077ef2bcb876fe29b16e
anurag8484 <anuragpoonia.ap@gmail.com>
2026-01-27 14:01:54 +0530

    add: basic stream talk with ai

--- be/dist/index.js
+++ be/dist/index.js
@@ -1,16 +1,14 @@
 import { OpenRouter } from "@openrouter/sdk";
 import * as dotenv from "dotenv";
-
 dotenv.config();
-
 const openRouter = new OpenRouter({
-  apiKey: process.env.CLAUDE_API_KEY,
+    apiKey: process.env.CLAUDE_API_KEY,
 });
-
 const result = await openRouter.callModel({
-  model: "anthropic/claude-sonnet-4.5",
-  input: "Create a todo application",
+    model: "anthropic/claude-sonnet-4.5",
+    input: "Create a todo application",
 });
 for await (const item of result.getTextStream()) {
-  console.log(item);
+    console.log(item);
 }
+//# sourceMappingURL=index.js.map
